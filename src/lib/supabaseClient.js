781e203ba855e66c5cfebaeb5ea3e03148a6aa08
kodzukye <lpark9142@gmail.com>
2025-05-13 08:42:06 -0400

    First commit

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Supabase URL and Key
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
